7e2edcb8bcf9706aec7a6b46764cd3b265d2b259
Dungewar <dungewar@gmail.com>
2025-04-25 10:17:09 -0700

    Do something

--- fibbonaci.py
+++ fibbonaci.py
@@ -1,18 +1,29 @@
-def fibb(n: int):
+from pickletools import long4
+
+
+def fibb(n: int) -> int:
     if n < 2:
         return n
     else:
         return fibb(n - 1) + fibb(n - 2)
 
 
-def fibb2(n: int):
-    cheese: list[int] = [0, 1]
+def fibb2(n: int) -> int:
+    fibb_numbers: list[int] = [0, 1]
     while True:
-        if len(cheese) > n:
-            return cheese[n]
+        if len(fibb_numbers) > n:
+            return fibb_numbers[n]
         else:
-            cheese.append(cheese[len(cheese) - 2] + cheese[len(cheese) - 1])
+            fibb_numbers.append(fibb_numbers[len(fibb_numbers) - 2] + fibb_numbers[len(fibb_numbers) - 1])
 
 
-for i in range(0, 50):
-    print(f"i={i} fibb={fibb2(i)}")
+def fibb3(n: int) -> int:
+    a, b, c = 0, 1, 0
+    while c < n:
+        a, b, c = a + b, a, c + 1
+    return a
+
+
+print(flush=False)
+for i in range(0, 10000):
+    print(f"i={i} fibb={fibb3(i)}")
